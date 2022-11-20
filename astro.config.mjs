24c752c382a251d6fa11cc8f8744ee81b41c2379
Joel Hooks <joelhooks@gmail.com>
2022-11-20 10:28:13 -0800

    seo shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,22 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 import react from "@astrojs/react";
+import remarkEmoji from 'remark-emoji';
 
-import remarkEmoji from 'remark-emoji'
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://joelhooks.com',
   integrations: [tailwind(), mdx({
     remarkPlugins: [remarkEmoji],
     extendPlugins: false
-  }), react()]
+  }), react(), sitemap(), prefetch(), partytown()]
 });
